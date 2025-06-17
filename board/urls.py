b63e71f44241303be9a6fbc5d209d39292aad721
Bokfortune <jibok815@naver.com>
2025-06-17 12:45:16 +0900

    1245

--- board/urls.py
+++ board/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index),  # board 앱의 index 뷰를 기본 URL로 설정
-    path('<int:question_id>/', views.detail),  # 질문 상세 페이지 URL
+    path('', views.index, name='question_list'),  # board 앱의 index 뷰를 기본 URL로 설정
+    path('<int:question_id>/', views.detail, name='question_detail'),  # 질문 상세 페이지 URL
 ]
